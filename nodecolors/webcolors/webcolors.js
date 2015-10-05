ecfe72a1513c50791f1f98ee7fa2847be15535a6
Paula Origlio <minplynn@aol.com>
2015-10-05 12:16:51 -0600

    finished node colors bonus exercises

--- nodecolors/webcolors/webcolors.js
+++ nodecolors/webcolors/webcolors.js
@@ -1,11 +1,14 @@
-var request = require('request');
-request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-  }
-  return body
-})
+// var fs = require("fs")
 
-console.log(body)
+// var request = require('request');
+// request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//   }
+// 	var content = body
+// 	var text = fs.writeFileSync("webcolors.json",body)
+// })
+
+var colors = require("./webcolors.json")
 
 var color = process.argv[2]
 
@@ -13,10 +16,10 @@ var colorSearch = function (color){
 	for(var i=0; i<colors.length; i++){
 		if(colors[i].name.toLowerCase() === color.toLowerCase()){
 			var colorCodes = colors[i].rgb
-			return colorCodes.r + " " + colorCodes.g + " " + colorCodes.b
+			return colorCodes.r + " " + colorCodes.g + " " + colorCodes.b + "\n" + colors[i].hex
 		}
-		else{
-			return "The color you entered is not a web color."
+		else if(i===colors.length-1){
+			return "The color '" + color + "' is not a color recognized by the web."
 		}
 	}
 }
